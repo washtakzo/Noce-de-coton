16a715c329700673fd8e18ec578b5d4f0242f09d
washtakzo <washtakzo@gmail.com>
2023-08-07 23:46:55 +0200

    paralax js test

--- components/Index/LoadingScreen/LoadingScreen.tsx
+++ components/Index/LoadingScreen/LoadingScreen.tsx
@@ -6,15 +6,22 @@ import weeding_logo from "../../../public/assets/images/weeding-logo.png";
 
 const LoadingScreen = () => {
   return (
-    <div className="fixed w-full h-full z-10">
-      <div
-        className={`absolute inline-block left-0 w-[50%] h-[100vh] bg-white ${styles.panel}`}
-      ></div>
-      <div
-        className={`absolute inline-block right-0 w-[50%] h-[100vh] bg-white ${styles.panel}`}
-      ></div>
+    <div className="fixed w-full h-full z-10 flex">
+      <div className="w-[50%] bg-transparent relative">
+        <div
+          className={`absolute inline-block right-0 w-full h-[100vh] bg-white ${styles.panel}`}
+        ></div>
+      </div>
+      <div className="w-[50%] bg-transparent relative">
+        <div
+          className={`absolute inline-block left-0 w-full h-[100vh] bg-white ${styles.panel}`}
+        ></div>
+      </div>
+
       <div className="absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center h-full w-full bg-transparent">
-        <div className={`relative w-72 h-72 object-cover ${styles.logo}`}>
+        <div
+          className={`relative w-[480px] h-[480px] object-cover ${styles.logo}`}
+        >
           <Image src={weeding_logo} alt="logo" />
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,61 @@
+import React from "react";
 import Head from "next/head";
 import LoadingScreen from "../components/Index/LoadingScreen/LoadingScreen";
+import Image from "next/image";
 
 export default function Home() {
+  // const titleRef = React.useRef<HTMLElement>(null);
+
+  React.useEffect(() => {
+    const titre = document.getElementById("image")! as HTMLTitleElement;
+
+    const intersectionObserver = new IntersectionObserver((entries, _) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+          // (entry.target as HTMLElement).style.transform = "rotate(90deg)";
+          // (entry.target as HTMLElement).addEventListener("scroll", () => {
+          //   const scrollY = window.scrollY;
+          //   console.log(scrollY);
+          // });
+
+          window.addEventListener("scroll", () => {
+            if (entry.intersectionRatio > 0) {
+              //--------------------------------------
+
+              //Scroll Y in document (px) : window.scrollY  //référence = top of the window
+
+              //Element Coord Y (px) : element.getBoundingClientRect().y
+              //Par rapport a la fenetre non pas tout le document
+              // = 0 lorsque le top de element aligné au top de la window
+
+              //Element Height (px) : element.offsetHeight  //fix
+              //Window Height (px) : window.innerHeight   //fix
+
+              const showCoord =
+                window.innerHeight - titre.getBoundingClientRect().y;
+              console.log({ showCoord });
+
+              let translateY = 0;
+              if (showCoord > 0) {
+                translateY = showCoord / 4;
+              }
+
+              //--------------------------------------
+
+              (
+                entry.target as HTMLElement
+              ).style.transform = `translateY(-${translateY}px)`;
+            }
+          });
+        }
+
+        //style = "transform: translate3d(0px, 15.8296px, 0px)";
+      });
+    });
+
+    intersectionObserver.observe(titre);
+  }, []);
+
   return (
     <div>
       <Head>
@@ -10,13 +64,44 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <LoadingScreen />
+      {/* <LoadingScreen /> */}
 
       <main>
-        <video width="1920" height="1080" autoPlay muted loop preload="auto">
-          <source src="assets/wedding.mp4" type="video/mp4" />
-          Video not supported
-        </video>
+        <div className="relative bg-red-100">
+          <video width="1920" height="1080" autoPlay muted loop preload="auto">
+            <source src="assets/videos/wedding.mp4" type="video/mp4" />
+            Video not supported
+          </video>
+          <h1 className="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center text-[60px] text-white font-serif">
+            Noce de coton
+          </h1>
+        </div>
+        <div className="h-screen w-screen bg-slate-300" />
+        <section className="flex h-[800px] p-4">
+          <div className="bg-red-50 w-[50%]">
+            <h2 id="titre" className="inline-block">
+              Bienvenue
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Temporibus dolorem sit molestias.
+            </p>
+          </div>
+          <div className=" bg-transparent w-[50%] flex justify-center items-center border-red-600 border-2">
+            <div className="relative w-full h-[1600px] border-green-600 border-2">
+              <Image
+                src="/../public/assets/images/__illustration1.jpg"
+                alt="illustration"
+                // width={1400}
+                // height={1600}
+                fill
+                className="h-[1400px] border-blue-600 border-2 object-cover"
+                id="image"
+              />
+            </div>
+          </div>
+        </section>
+        <div className="h-screen w-screen bg-red-100" />
       </main>
 
       <footer></footer>
