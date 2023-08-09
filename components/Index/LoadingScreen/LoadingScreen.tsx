9d96e7f69356c8e0c4ef92daf4549e76714837c2
washtakzo <washtakzo@gmail.com>
2023-08-09 13:41:17 +0200

    Fix : LoadingScreen position fixed

--- components/Index/LoadingScreen/LoadingScreen.tsx
+++ components/Index/LoadingScreen/LoadingScreen.tsx
@@ -7,7 +7,7 @@ import weeding_logo from "../../../public/assets/images/weeding-logo.png";
 const LoadingScreen = () => {
   return (
     <div
-      className={`fixed w-full h-full z-10 flex justify-center items-center ${styles["open-animation"]}`}
+      className={`fixed top-0 left-0 bottom-0 right-0 bg-white z-10 flex justify-center items-center ${styles["open-animation"]}`}
     >
       <div
         className={`relative w-[480px] h-[480px] object-cover ${styles.logo}`}
