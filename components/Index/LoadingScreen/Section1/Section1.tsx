ea07f141d4511d410817cec58f9674e2e821d475
washtakzo <washtakzo@gmail.com>
2023-08-14 14:59:54 +0200

    Section with window image in progress

--- components/Index/LoadingScreen/Section1/Section1.tsx
+++ components/Index/LoadingScreen/Section1/Section1.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 
 import styles from "./Section1.module.css";
+import Image from "next/image";
+
+const img1 =
+  "https://static.wixstatic.com/media/c49135_fe230beccf0a4b6fbba1941e2660ff99~mv2.png/v1/fill/w_930,h_973,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/c49135_fe230beccf0a4b6fbba1941e2660ff99~mv2.png";
 
 const Section1 = () => {
   React.useEffect(() => {
-    const imageEl = document.getElementById("image")! as HTMLTitleElement;
-
-    const imageContainerEl = document.getElementById(
+    const imgEl = document.getElementById("image")! as HTMLImageElement;
+    const imgContainerEl = document.getElementById(
       "image-container"
-    )! as HTMLDivElement;
+    )! as HTMLImageElement;
 
     const intersectionObserver = new IntersectionObserver((entries, _) => {
       entries.forEach((entry) => {
         if (entry.intersectionRatio > 0) {
+          (entry.target as HTMLElement).addEventListener("scroll", () => {
+            const scrollY = window.scrollY;
+            console.log(scrollY);
+          });
+
           window.addEventListener("scroll", () => {
             if (entry.intersectionRatio > 0) {
               //--------------------------------------
@@ -26,20 +34,37 @@ const Section1 = () => {
               //Element Height (px) : element.offsetHeight  //fix
               //Window Height (px) : window.innerHeight   //fix
 
-              const showCoord =
-                window.innerHeight - imageContainerEl.getBoundingClientRect().y;
-              console.log({ showCoord });
+              // TODO:récupérer la position de l'image en pourcentage par rapport au top de la window
+              const imgContainerHeight = imgContainerEl.offsetHeight;
 
-              let translateY = 0;
-              if (showCoord > 0) {
-                translateY = showCoord / 4;
-              }
+              const imageHeight = imgEl.offsetHeight;
 
-              //--------------------------------------
+              const containerPosition =
+                // imgContainerEl.getBoundingClientRect().y;  //save
+                imgContainerEl.getBoundingClientRect().y + imgContainerHeight;
 
-              //   (
-              //     entry.target as HTMLElement
-              //   ).style.transform = `translateY(-${translateY}px)`;
+              // let containerPositionPrCent = 100 - (containerPosition * 100) / window.innerHeight;  //save
+              let containerPositionPrCent =
+                100 -
+                (containerPosition * 100) /
+                  (window.innerHeight + imgContainerHeight);
+
+              if (containerPositionPrCent < 0) containerPositionPrCent = 0;
+              if (containerPositionPrCent > 100) containerPositionPrCent = 100;
+              console.log({ containerPosition });
+
+              const maxTranslateY =
+                100 - (imgContainerHeight * 100) / imageHeight;
+
+              let translateYY = (maxTranslateY * containerPositionPrCent) / 100;
+
+              if (translateYY > maxTranslateY) translateYY = maxTranslateY;
+
+              console.log({ containerPositionPrCent });
+
+              (
+                entry.target as HTMLElement
+              ).style.transform = `translateY(${translateYY}%)`;
             }
           });
         }
@@ -47,32 +72,40 @@ const Section1 = () => {
         //style = "transform: translate3d(0px, 15.8296px, 0px)";
       });
     });
-    intersectionObserver.observe(imageEl);
-  });
+
+    intersectionObserver.observe(imgEl);
+  }, []);
+
   return (
-    <section className={styles.wrapper}>
-      <div className={"bg-green-50 h-screen flex-grow-1"}>
-        <h2 id="titre" className="inline-block">
-          Bienvenue
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          dolorem sit molestias.
+    <section className="h-[480px] w-full bg-slate-50 flex justify-center items-center">
+      <div className="flex flex-col flex-1 justify-center items-center gap-6">
+        <div className="flex justify-center items-center flex-col gap-2">
+          <h2 className="text-[1.5rem] font-serif uppercase">
+            <span className="text-[3rem]">Q</span>ui sommes-nous ?
+          </h2>
+          <span className="w-[15%] h-[1px] bg-slate-800" />
+          <h3 className="text-xl font-normal font-serif">
+            Ensemble, créons lunique
+          </h3>
+        </div>
+        <p className="w-[50%]">
+          AS Luxury est une agence événementielle de prestige spécialisée dans
+          la production dévénements et de mariages extraordinaires, la
+          scénographie et la décoration florale, en France et dans le monde
+          entier.
         </p>
+        <a href="">En savoir plus</a>
       </div>
-      <div id="image-container" className={styles.header}>
-        <div id="image" className={`${styles["image-container"]}`}>
-          {/* <img
-            src="https://assets.vogue.com/photos/6457fe35c943a2672e3e6c65/16:9/w_1920%2Cc_limit/vg-125.jpg"
-            alt="test"
-            className={styles.image}
-          /> */}
-        </div>
-        <div>
-          <button className={styles.button}>Button</button>
-          <button className={styles.button2}>Button 2</button>
-          <button className={styles.button3}>Button 3</button>
-        </div>
+      <div
+        id="image-container"
+        className="relative flex-1 h-full bg-green-100 overflow-hidden"
+      >
+        <img
+          id="image"
+          className="object-contain absolute bottom-0 left-0 w-full bg-blue-300 scale-[1]"
+          src={img1}
+          alt="illustration mariage"
+        />
       </div>
     </section>
   );
