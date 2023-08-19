a9fce39ec841e38355e7abb6a2eb395909b9e8c6
washtakzo <washtakzo@gmail.com>
2023-08-19 22:16:50 +0200

    update SectionParallax

--- components/Index/SectionStayTuned/SectionStayTuned.tsx
+++ components/Index/SectionStayTuned/SectionStayTuned.tsx
@@ -10,7 +10,7 @@ const SectionStayTuned = () => {
       link=""
       image="https://images.unsplash.com/photo-1530092285049-1c42085fd395?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
       paragraphe="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, aperiam expedita aliquid magni deleniti iure maxime rerum a eius repellendus ea dolore voluptatem cumque perspiciatis tempora excepturi adipisci nesciunt ex?"
-      reverse={false}
+      reverse={true}
     />
   );
 };
--- components/UI/SectionParallax/SectionParallax.tsx
+++ components/UI/SectionParallax/SectionParallax.tsx
@@ -43,42 +43,28 @@ const SectionParallax = ({
     });
   }, [imageId]);
 
-  const InfoPart = () => (
-    <div className="flex flex-col flex-1 justify-center items-center gap-6">
-      <SectionTitle title={title} subTitle={subTitle} />
-      <P>{paragraphe}</P>
-      <A drawingIsDisplay={true}>En savoir plus</A>
-    </div>
-  );
-
-  const ImagePart = () => (
-    <div
-      id={imageId + "-container"}
-      className="relative flex-1 h-full min-h-[200px] w-full overflow-hidden"
-    >
-      <img
-        id={imageId}
-        className="object-contain absolute bottom-0 left-0 w-full"
-        src={image}
-        alt={title}
-      />
-    </div>
-  );
-
   return (
-    <section className="h-[620px] w-full bg-white flex justify-center items-center flex-col gap-8 lg:flex-row lg:h-[420px] xl:h-[600px]">
-      {!reverse && (
-        <>
-          <InfoPart />
-          <ImagePart />
-        </>
-      )}
-      {reverse && (
-        <>
-          <ImagePart />
-          <InfoPart />
-        </>
-      )}
+    <section
+      className={`h-[620px] w-full bg-white flex justify-center items-center flex-col gap-8 ${
+        reverse ? "lg:flex-row-reverse" : "lg:flex-row"
+      } lg:h-[420px] xl:h-[600px]`}
+    >
+      <div className="flex flex-col flex-1 justify-center items-center gap-6">
+        <SectionTitle title={title} subTitle={subTitle} />
+        <P>{paragraphe}</P>
+        <A drawingIsDisplay={true}>En savoir plus</A>
+      </div>
+      <div
+        id={imageId + "-container"}
+        className="relative flex-1 h-full min-h-[200px] w-full overflow-hidden"
+      >
+        <img
+          id={imageId}
+          className="object-contain absolute bottom-0 left-0 w-full"
+          src={image}
+          alt={title}
+        />
+      </div>
     </section>
   );
 };
